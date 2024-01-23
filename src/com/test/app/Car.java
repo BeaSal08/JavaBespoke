ab90e65680a56787e4ce5031c0939fe021a070cf
BeaSal08 <beasal08@gmail.com>
2024-01-23 14:04:26 +0000

    Parameterized Constructor - example

--- src/com/test/app/Car.java
+++ src/com/test/app/Car.java
@@ -25,13 +25,5 @@ public class Car {
         // initialized parameters:
         this.name = name;
         this.yearOfMake = yearOfMake;
-
-        // runs at 1st use lang
-        if (yearOfMake < 2000) {
-            System.out.println("Car is very old");
-        }
-        else {
-            System.out.println("Car is new");
-        }
     }
 }
