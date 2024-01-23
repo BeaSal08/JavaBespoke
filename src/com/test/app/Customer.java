e3ee868eebe93e50cb26f848f468bf3566c707b4
BeaSal08 <beasal08@gmail.com>
2024-01-23 09:59:42 +0000

    changed access modified from default --> private, fields cannot be accessed outside the class w/o getters and setters

--- src/com/test/app/Customer.java
+++ src/com/test/app/Customer.java
@@ -3,11 +3,11 @@ package com.test.app;
 // Class names start with capital letters
 public class Customer {
     //set up fields for the Customer (bc it is a blueprint)
-    int age;
-    long phoneNumber;
-    boolean isKycDone;
-    char areaCode;
-    String name;
+    private int age;
+    private long phoneNumber;
+    private boolean isKycDone;
+    private char areaCode;
+    private String name;
 
 
 }
