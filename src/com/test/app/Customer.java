5522e32985d70e5add4787301ac7c6390d3d6438
BeaSal08 <beasal08@gmail.com>
2024-01-23 09:49:55 +0000

    Cretaed sample fields in customer class

--- src/com/test/app/Customer.java
+++ src/com/test/app/Customer.java
@@ -1,4 +1,9 @@
 package com.test.app;
 
+// Class names start with capital letters
+// variables are in camelCase
 public class Customer {
+    //set up fields for the Customer (bc it is a blueprint)
+    int age;
+    long phoneNumber;
 }
