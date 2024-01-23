35ba2e2838cb7f141c119ff885e5270c87fdd089
BeaSal08 <beasal08@gmail.com>
2024-01-23 13:32:55 +0000

    set values for the obj, fixed sout

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
         // Instantiate a class to make an object
         Customer customer = new Customer();
 
+        // Set values of different fields of the customer
         customer.setAge(30);
         customer.setName("Deba");
         customer.setPhoneNumber(454545);
         customer.setKycDone(false);
         customer.setAreaCode('W');
 
-        System.out.println("age = " + customer.getAge());
-        System.out.println("phoneNumber = " + customer.getPhoneNumber());
-        System.out.println("isKycDone = " + customer.isKycDone());
-        System.out.println("areaCode = " + customer.getAreaCode());
+        System.out.println("==== Customer Details ====" +
+                "\nAge: " + customer.getAge() +
+                "\nphoneNumber: " + customer.getPhoneNumber() +
+                        "\nisKycDone: " + customer.isKycDone() +
+                        "\nareaCode: " + customer.getAreaCode()
+                );
     }
 }
