4fc5dfbbacbadc671f192657dc794b0a67c460d9
BeaSal08 <beasal08@gmail.com>
2024-01-23 09:56:38 +0000

    Initialized a variable in main - simple

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         int age = 21;
         long phoneNumber = 45454545;
         boolean isKycDone = true;
+        char areaCode = 'A';
 
         System.out.println("age = " + age);
         System.out.println("phoneNumber = " + phoneNumber);
         System.out.println("isKycDone = " + isKycDone);
+        System.out.println("areaCode = " + areaCode);
     }
 }
