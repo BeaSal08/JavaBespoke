b0e67c81970a6d422308bd769a4f7aa67a96abb8
BeaSal08 <beasal08@gmail.com>
2024-01-23 09:58:11 +0000

    Start of Encapsulation Example, moved fields in main --> customer class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // variables are in camelCase
-        int age = 21;
-        long phoneNumber = 45454545;
-        boolean isKycDone = true;
-        char areaCode = 'A';
+
 
         System.out.println("age = " + age);
         System.out.println("phoneNumber = " + phoneNumber);
--- src/com/test/app/Customer.java
+++ src/com/test/app/Customer.java
@@ -3,4 +3,11 @@ package com.test.app;
 // Class names start with capital letters
 public class Customer {
     //set up fields for the Customer (bc it is a blueprint)
+    int age;
+    long phoneNumber;
+    boolean isKycDone;
+    char areaCode;
+    String name;
+
+
 }
