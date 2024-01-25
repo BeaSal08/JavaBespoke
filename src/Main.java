b68f8af98e2cfff737170b4ef53a53e87f69ff13
BeaSal08 <beasal08@gmail.com>
2024-01-25 07:10:16 +0000

    code clean up comments

--- src/Main.java
+++ src/Main.java
@@ -4,54 +4,6 @@ import com.test.app.CustomerService;
 
 public class Main {
     public static void main(String[] args) {
-//        // Instantiate a class to make an object
-//        Customer customer = new Customer(); // uses the default constructor bc we havent created a constructor in the customer class
-//
-//        // Set values of different fields of the customer
-//        customer.setAge(30);
-//        customer.setName("Deba");
-//        customer.setPhoneNumber(454545);
-//        customer.setKycDone(false);
-//        customer.setAreaCode('W');
-//
-//        System.out.println("==== Customer Details ====" +
-//                "\nAge: " + customer.getAge() +
-//                "\nphoneNumber: " + customer.getPhoneNumber() +
-//                        "\nisKycDone: " + customer.isKycDone() +
-//                        "\nareaCode: " + customer.getAreaCode()
-//                );
-//
-//        // Create another obj (has no relation to 1st customer)
-//        Customer customer1 = new Customer();
-//
-//        customer1.setAge(40);
-//        customer1.setName("Sam");
-//        customer1.setPhoneNumber(636363);
-//        customer1.setKycDone(true);
-//        customer1.setAreaCode('A');
-//
-//        System.out.println("==== Customer Details ====" +
-//                "\nAge: " + customer1.getAge() +
-//                "\nphoneNumber: " + customer1.getPhoneNumber() +
-//                "\nisKycDone: " + customer1.isKycDone() +
-//                "\nareaCode: " + customer1.getAreaCode()
-//        );
-//
-//        // Constructors:
-//        Car car = new Car("Mini Cooper", 1999);
-//        System.out.println("==== Car Details ====" +
-//                "\nAge: " + car.getName() +
-//                "\nphoneNumber: " + car.getYearOfMake()
-//        );
-//
-//        car.setName("Honda");
-//        car.setYearOfMake(2003);
-//
-//        System.out.println("==== Updated Car Details ====" +
-//                "\nAge: " + car.getName() +
-//                "\nphoneNumber: " + car.getYearOfMake()
-//        );
-
         // Polymorphism - Overloading
         Customer customer = new Customer(); // Creates a customer object whose fields are set
 
@@ -63,7 +15,7 @@ public class Main {
         customer.setAreaCode('W');
         customer.setCreditRating(800);
 
-        // Initialize customer service so you can use it's functions
+        // Initialize customer service so you can use its functions
         // this will show you 2 diff options for addPrivilege bc of overloading we did in CustomerService.java
         CustomerService customerService = new CustomerService();
 //        customerService.addPrivilege(customer.getCreditRating());
