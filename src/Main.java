220627f9e6a1a51d3075a96a66cff0d40c63405c
BeaSal08 <beasal08@gmail.com>
2024-01-25 09:27:42 +0000

    Array Examples

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // 1D
         String branchNames[] = {"Manila", "Paris", "Mumbai"};
         System.out.println("1st branch, index 0: " + branchNames[0]);
+        System.out.println("Array length: " + branchNames.length);
 
         // Multi-D, array of arrays
         int marks[][] = {
@@ -13,9 +14,11 @@ public class Main {
                 {98, 56, 79, 90, 92},
                 {78, 88, 56, 70, 99}
         };
+        System.out.println("marks[1][2]: " + marks[1][2]); //gets 2nd row, 3rd col = 79
+        System.out.println("Array length: " + marks.length);
 
         int result[][] = new int[3][5];
-        System.out.println("marks[1][2]: " + marks[1][2]); //gets 2nd row, 3rd col = 79
+        System.out.println("Array length: " + result.length);
 
     }
 }
