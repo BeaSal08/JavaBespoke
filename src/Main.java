5e2e6998c59424819fa6461c6e58cf8d7bcfd74f
BeaSal08 <beasal08@gmail.com>
2024-01-25 11:00:49 +0000

    for loop example

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,26 @@ import com.test.app.*;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        customer.setName("Deba");
-        customer.setAge(31);
-        customer.setCreditRating(600);
-        customer.setBankAccountType("Saving");
+//        Customer customer = new Customer();
+//        customer.setName("Deba");
+//        customer.setAge(31);
+//        customer.setCreditRating(800);
+//        customer.setBankAccountType("Saving");
+//
+//        Loan loan = new Loan();
+//        loan.setLoanAmount(20000);
+//        loan.setLoanTypes(LoanTypes.HOME);
+//        customer.setLoan(loan);
+//
+//        HomeLoanService homeLoanService = new HomeLoanService();
+//        homeLoanService.applyLoan(customer);
 
-        Loan loan = new Loan();
-        loan.setLoanAmount(20000);
-        loan.setLoanTypes(LoanTypes.HOME);
-        customer.setLoan(loan);
+        //FOR LOOPS
+        String customers[] = {"Deba", "Sam", "Yasmin"};
 
-        HomeLoanService homeLoanService = new HomeLoanService();
-        homeLoanService.applyLoan(customer);
+        for (int i = 0; i < customers.length; i++)
+        {
+            System.out.println("Customer applied for loan: " + customers[i]);
+        }
     }
 }
