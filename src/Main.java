c7095d70f7ba746fc5e73bbd4c073ec3ea41d8de
BSal_Windows <ma.salazar@globe.com.ph>
2024-02-05 14:33:36 +0800

    Concatination

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        CustomerService customerService = new CustomerService();
-        ArrayList<Customer> customers = customerService.createCustomerList();
-        customerService.openBankAccount(customers);
+//        CustomerService customerService = new CustomerService();
+//        ArrayList<Customer> customers = customerService.createCustomerList();
+//        customerService.openBankAccount(customers);
+
+        String s1 = "Hello World";
+        String s2 = "Goodbye World";
+
+        System.out.println("=== String Methods ===");
+        // Concatination
+        String s3 = s1.concat(s2) ;
+        String s4 = s1 + s2;
+        System.out.println("s3 = " + s3);
+        System.out.println("s4 = " + s4);
 
     }
 }
