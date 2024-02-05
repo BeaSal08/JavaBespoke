c07ef9c017c79c5a3a9701d557675c439e5dc888
BSal_Windows <ma.salazar@globe.com.ph>
2024-02-05 14:21:21 +0800

    added timestamp for quiz ans

--- src/com/test/app/CarLoanService.java
+++ src/com/test/app/CarLoanService.java
@@ -2,4 +2,5 @@ package com.test.app;
 
 public abstract class CarLoanService extends LoanService {
     // ANS TO QUIZ: FALSE bc a class CANNOT  inherit from multiple abstract classes
+    //Java D4, 29 min mark
 }
